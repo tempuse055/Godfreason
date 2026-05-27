c8e64aaeb36d5d85227f22ddfe18df80444f4eb8
tempuse055 <tempuse055@gmail.com>
2026-05-27 20:05:07 +0530

    drx.py

--- drx.py
+++ drx.py
@@ -18,7 +18,7 @@ else:
 
 bot = telebot.TeleBot(config['token'])
 # Agar API isi VPS par hai toh '127.0.0.1' use karein, varna VPS ka IP dalein
-API_URL = "http://34.126.208.96:8080/hit" 
+API_URL = os.getenv('API_URL', 'http://localhost:8080/hit')
 AUTH_TOKEN = "DRX_POWER_ULTRA_V4"
 
 # Database files
